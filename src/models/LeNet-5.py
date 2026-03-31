0967ac001571f3125610e2b44430369d1b3adea9
KhoiHai <lenhatkhoittmt2005@gmail.com>
2026-03-31 19:43:08 +0700

    upload LeNet-5

---
+++ src/models/LeNet-5.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LeNet5(nn.Module):
+    '''
+    LeNet5 Architecture
+    '''
+    def __init__(self, in_channels: int = 1, num_classes: int = 10):
+        '''
+        in_channel (int): Channel in the input
+        num_classes (int): Number of classes for classification
+        '''
+        super().__init__()
+
+        # C1: Convolution Layer
+        self.C1 = nn.Conv2d(in_channels = in_channels, out_channels = 6, kernel_size = 5)
+
+        # C2: Convolution Layer
+        self.C2 = nn.Conv2d(in_channels = 6, out_channels = 16, kernel_size = 5)
+
+        # C3: Convolution Layer
+        self.C3 = nn.Conv2d(in_channels = 16, out_channels = 120, kernel_size = 5)
+
+        # Fully connected layer
+        self.fc1 = nn.Linear(in_features = 120, out_features = 84)
+        self.fc2 = nn.Linear(in_features = 84, out_features = num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.C1(x)) 
+        x = F.avg_pool2d(x, 2)
+
+        x = F.relu(self.c2(x))
+        x = F.avg_pool2d(x, 2)
+
+        x = F.relu(self.c3(x))
+        x = torch.flatten(x, 1)
+
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return 
